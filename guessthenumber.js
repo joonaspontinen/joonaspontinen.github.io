03f5ef4a9569312a08adf0f0cbd392503a7e50c6
joonaspontinen <joonas.pontinen@aalto.fi>
2017-02-27 12:49:08 +0200

    asdadawdwaddawd
    adasdad

--- guessthenumber.js
+++ guessthenumber.js
@@ -14,11 +14,11 @@ function compareNumbers(first, second) {
 function Guess(target) {
     'use strict';
     if (compareNumbers(target, correctNumber)) {
-        
+        window.alert("You win");
     } else {
         
         if (target > 0 && target < 11) {
-            
+            window.alert("Wrong");
         } else {
             window.alert("Please choose a number between 1 and 10");
         }

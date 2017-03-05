97d963d5a8d4c5d8db08a6c748223d19202f2077
joonaspontinen <joonas.pontinen@aalto.fi>
2017-03-05 20:35:56 +0200

    asdasd
    asdasdasd

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,7 @@
 
 
 var index = 0;
-
+var keepScrolling = true;
 
 window.onload = function () {
     'use strict';
@@ -9,4 +9,49 @@ window.onload = function () {
         console.log(data);
         $('#otsikko').html(data.uutiset[index].otsikko);
     });
+};
+
+function display() {
+    'use strict';
+    $.getJSON("pontinj1-589288.firebaseio.com/.json", function (data) {
+        $('#otsikko').html(data.uutiset[index].otsikko);
+        $('#paivamaara').html(data.uutiset[index].paivamaara);
+        $('#sisalto').html(data.uutiset[index].sisalto);
+    });
+}
+
+function nextSlide() {
+    'use strict';
+    if (index < 2) {
+        index += 1;
+        display();
+    } else {
+        index = 0;
+        display();
+    }
+}
+
+function previousSlide() {
+    'use strict';
+    if (index > 0) {
+        index -= 1;
+        display();
+    } else {
+        index = 2;
+        display();
+    }
+}
+
+function stopSlide() {
+    'use strict';
+    keepScrolling = !keepScrolling;
+}
+
+function changeArticle() {
+    'use strict';
+    while (keepScrolling) {
+        setInterval(function () {
+            nextSlide();
+        }, 1000);
+    }
 }

26d6cb873e17831c407107276bd82f94c49e85fc
joonaspontinen <joonas.pontinen@aalto.fi>
2017-04-09 15:36:12 +0300

    asd

--- animation.js
+++ animation.js
@@ -1,17 +1,11 @@
-$(document).ready(function () {
-    var canvas = document.getElementById('myCanvas');
+
+$(document).ready(function(){
+    var canvas = document.createElement('canvas');
     var ctx = canvas.getContext('2d');
     canvas.width = 400;
     canvas.height = 400;
     document.body.appendChild(canvas);
     
-    
-    
-      function main(){
-        update();
-        drawPlayer(ctx);
-        requestAnimationFrame(main);
-    }
     var player = {
         x: 200,
         y: 200,
@@ -19,60 +13,79 @@ $(document).ready(function () {
         h: 40,
         speed: 10
     };
-function move(direction) {
-    'use strict';
-    switch (direction) {
-    case "left":
-        player.x -= player.speed;
-        if (player.x < 20) {
-            player.x = 20;
+    
+    var keysDown = {};
+    
+    function movePlayer(direction){
+        switch (direction){
+            case "left":
+                player.x -= player.speed;
+                if(player.x<20){
+                    player.x = 20
+                }
+                break;
+            case "right":
+                player.x += player.speed;
+                if(player.x>380){
+                    player.x = 380
+                }
+                break;
+            case "up":
+                player.y -= player.speed;
+                if(player.y<20){
+                    player.y = 20
+                }
+                break;
+            case "down":
+                player.y += player.speed;
+                if(player.y>380){
+                    player.y = 380
+                }
+                break;
         }
-        break;
-    case "right":
-        player.x += player.speed;
-        if (player.x > 380) {
-            player.x = 380;
-        }
-        break;
-    case "up":
-        player.y -= player.speed;
-        if (player.y < 20) {
-            player.y = 20;
-        }
-        break;
-    case "down":
-        player.y += player.speed;
-        if (player.y > 380) {
-            player.y = 380;
-        }
-        break;
     }
-}
-
-function changeSpeed(direction) {
-    'use strict';
-    switch (direction) {
-    case "up":
-        player.speed += 20;
-        break;
-    case "down":
-        player.speed -= 20;
-        break;
+    
+    window.addEventListener('keydown', function(e){
+        keysDown[e.keyCode] = true;
+    });
+    
+    window.addEventListener('keyup', function(e){
+        delete keysDown[e.keyCode];
+    })
+    
+    function drawPlayer(context) {
+        var x = player.x - (player.w/2);
+        var y = player.y - (player.h/2);
+        context.fillStyle = '#ff0000';
+        context.fillRect(x,y,player.w,player.h);
+    };
+    
+    var render = function(){
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0,0,400,400);
+    
+    drawPlayer(ctx);
+    };
+    
+    function update(){
+        if(38 in keysDown){
+            movePlayer('up');
+        }
+        if(40 in keysDown){
+            movePlayer('down');
+        }
+        if(37 in keysDown){
+            movePlayer('left');
+        }
+        if(39 in keysDown){
+            movePlayer('right');
+        }
+    };
+    
+    function main(){
+        update();
+        render();
+        requestAnimationFrame(main);
     }
-}
-function reset() {
-    'use strict';
-    player.x = 200;
-    player.y = 200;
-    player.w = 40;
-    player.h = 40;
-    player.speed = 10;
-}
-function drawPlayer(context) {
-    'use strict';
-    var a = player.x - (player.w / 2);
-    var b = player.y - (player.h / 2);
-    context.fillStyle = '#ff0000';
-    context.fillRect(a, b, player.w, player.h);
-}
     main();
+                      }

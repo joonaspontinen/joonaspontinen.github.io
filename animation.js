fc0c95bdc28bb712277c42b6acb088f50298d57c
joonaspontinen <joonas.pontinen@aalto.fi>
2017-04-09 16:10:34 +0300

    asdasd
    adasdasdasdsad

--- animation.js
+++ animation.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    'use strict';
     var canvas = document.createElement('canvas');
     var ctx = canvas.getContext('2d');
     canvas.width = 400;
@@ -15,73 +16,73 @@ $(document).ready(function() {
     
     var keysDown = {};
     
-    function movePlayer(direction){
-        switch (direction){
-            case "left":
-                player.x -= player.speed;
-                if(player.x<20){
-                    player.x = 20;
-                }
-                break;
-            case "right":
-                player.x += player.speed;
-                if(player.x>380){
-                    player.x = 380;
-                }
-                break;
-            case "up":
-                player.y -= player.speed;
-                if(player.y<20){
-                  player.y = 20;
-                }
+    function movePlayer(direction) {
+        switch (direction) {
+        case "left":
+            player.x -= player.speed;
+            if (player.x < 20) {
+                player.x = 20;
+            }
+            break;
+        case "right":
+            player.x += player.speed;
+            if (player.x > 380) {
+                player.x = 380;
+            }
+            break;
+        case "up":
+            player.y -= player.speed;
+            if (player.y < 20) {
+                player.y = 20;
+            }
                 
-                break;
-            case "down":
-                player.y += player.speed;
-                if(player.y>380){
-                    player.y = 380;
-                }
-                break;
+            break;
+        case "down":
+            player.y += player.speed;
+            if (player.y > 380) {
+                player.y = 380;
+            }
+            break;
         }
     }
     
-    window.addEventListener('keydown', function(e){
+    window.addEventListener('keydown', function (e) {
         keysDown[e.keyCode] = true;
     });
     
-    window.addEventListener('keyup', function(e){
+    window.addEventListener('keyup', function (e) {
         delete keysDown[e.keyCode];
     });
     
     function drawPlayer(context) {
-        var x = player.x - (player.w/2);
-        var y = player.y - (player.h/2);
+        var x = player.x - (player.w / 2);
+        var y = player.y - (player.h / 2);
         var pelaaja = new Image();
-        pelaaja.src ="pokemon.png";
+        pelaaja.src = "pokemon.png";
         context.drawImage(pelaaja, x, y);
-    };
+    }
     
-    var render = function() {
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(0,0,400,400);
+    var render = function () {
+            ctx.fillStyle = '#000000';
+            ctx.fillRect(0, 0, 400, 400);
     
-    drawPlayer(ctx);
-    };
+            drawPlayer(ctx);
+        };
     
-    function update(){
-        if(38 in keysDown){
+    function update() {
+        if (38 in keysDown) {
             movePlayer('up');
         }
-        if(40 in keysDown){
+        if (40 in keysDown) {
             movePlayer('down');
         }
-        if(37 in keysDown){
+        if (37 in keysDown) {
             movePlayer('left');
         }
-        if(39 in keysDown){
+        if (39 in keysDown) {
             movePlayer('right');
         }
-    };
+    }
     
     function main(){
         update();
